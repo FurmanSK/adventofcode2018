04a93bd804fe8d59d346746d8766f55b7e1e75b5
Wes Forman <wesforman@gmail.com>
2018-12-19 13:11:46 -0500

    finished day 5

--- day5/solution.py
+++ day5/solution.py
@@ -1,11 +1,6 @@
 import sys
 import re
-
-sys.setrecursionlimit(50000)
-f = open('day5/input.txt', 'r')
-data = f.readline()
-# sample data for testing
-# data = "dabAcCaCBAcCcaDA"
+from multiprocessing import Process
 
 def checkchars(s):
     #for i, character in enumerate(s):
@@ -32,21 +27,49 @@ def checkchars(s):
                     del s[i]
                     # checkchars(s)
                     return False
+#looper to prevent stack growing too large.
+def looper(s, msg):
+    n = list(s)
+    print("In thread")
+    while(True):
+        if(checkchars(n)):
+            break
+    print(msg)
+    print("Length of string is now " + str(len(n))+"\n")
 
 
-# If an upper and lower letter are the same letter (type) but different polarity (upper/lower)
-# then destroy them (remove them) from the string.
-d = list(data)
-# while(True):
-#     if (checkchars(d)):
-#         break
-# print("Number left = ", len(d))
+if __name__ == '__main__':
 
-d2 = list(data)
+    sys.setrecursionlimit(50000)
+    f = open('day5/input.txt', 'r')
+    data = f.readline()
+    # sample data for testing
+    # data = "dabAcCaCBAcCcaDA"
 
-while(True):
-    # need to remove each 26 letters at one time and then run the checkchars function on each one to see which one gives us the smallest lenght.
-    # 
+    # Day 5 solution 1
+    # If an upper and lower letter are the same letter (type) but different polarity (upper/lower)
+    # then destroy them (remove them) from the string.
+    # Comment out if only looking for solution 2 of day 5
+    d = list(data)
+    while(True):
+        if (checkchars(d)):
+            break
+    print("Number left = ", len(d))
+
+    # Day 5 solution 2
+    # Used multiprocess library to fork off 26 children to process at the same time
+    # This saves time especially with my Ryzen 16 Thread beast!
+    lowerChar = 'a'
+    upperChar = 'A'
+    length = 0
+    # need to remove each 26 letters at one time and then run the checkchars function on each one to see which one gives us the smallest lenght. 
     for i in range(26):
-        t = list(re.sub('[aA]', '', data))
+        searchText = "["+lowerChar+upperChar+"]"
+        msg = "Did search with removing of " + searchText + "\n"
+        t = re.sub(searchText, '', data)
+        lowerChar = chr(ord(lowerChar) + 1)
+        upperChar = chr(ord(upperChar) + 1)
+
+        p = Process(target=looper, args=[t, msg])
+        p.start()
     
